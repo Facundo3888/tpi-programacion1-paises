91e57c81503972d4ade2dc0af0bc1dbb1f308a61
Facundo3888 <facundourielrodriguez8@gmail.com>
2026-06-06 19:30:44 -0300

    se detallan funciones necesarias

--- logica_paises.py
+++ logica_paises.py
@@ -1,1 +1,15 @@
 #Aca van las funciones que hacen los procesos matematicos
+
+#def agregar_pais(lista_paises)
+
+#actualizar_pais(lista_paises)
+
+#buscar_pais_nombre(lista_paises)
+
+#filtrar_paises(lista_paises)
+#Esta quizas deberia divirse en 3 subfunciones ya que la consigna menciona 3 tipos de filtrado
+
+#ordenar_paises(lista_paises)
+#Esta quizas deberia tener una subfuncion de bubble sort ya que pide ascendente y descendente
+
+#mostrar_estadisticas
